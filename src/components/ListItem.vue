93a8fea3835f756fedff6271c75565d75ed83f79
coding-jhuunn <codingatjhunn@gmail.com>
2024-10-16 14:51:09 +0800

    learn avoid unnecessary update

---
+++ src/components/ListItem.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onUpdated } from "vue";
+const props = defineProps({
+  id: Number,
+  msg: String,
+  isActive: Boolean,
+});
+onUpdated(() => {
+  console.log("updating" + props.id);
+});
+</script>
+<template>
+  <li>
+    <span v-if="isActive">-></span>
+    {{ msg }}
+  </li>
+</template>
